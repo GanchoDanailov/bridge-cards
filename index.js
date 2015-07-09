d5b6568f1e5e81d64521098602569f42e377a008
Zhivko Draganov <zhivko.draganov@gmail.com>
2015-07-09 15:52:22 +0300

    Add navigation and list view option

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:suit', function (req, res) {
-  var ranks = ['ace', 'king', 'queen', 'jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two'];
-
   var cardsWithSuit = [];
   ranks.forEach(function (rank) {
     cardsWithSuit.push({
@@ -29,12 +27,52 @@ app.get('/:suit', function (req, res) {
 });
 
 app.get('/:suit/:rank', function (req, res) {
+  var currentCard = {
+    suit: req.params.suit,
+    rank: req.params.rank
+  };
+
   res.render('game', {
-    card: {
-      suit: req.params.suit,
-      rank: req.params.rank
-    }
+    card: currentCard,
+    next: nextCard(currentCard),
+    prev: prevCard(currentCard)
   });
 });
+
+var ranks = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
+
+function prevCard(currentCard) {
+  var currentRankPossition = ranks.indexOf(currentCard.rank);
+  var rankIndex = null;
+
+  if (currentRankPossition === 0) {
+    rankIndex = 12;
+  }
+  else {
+    rankIndex = currentRankPossition - 1;
+  }
+
+  return {
+    suit: currentCard.suit,
+    rank: ranks[rankIndex]
+  };
+}
+
+function nextCard(currentCard) {
+  var currentRankPossition = ranks.indexOf(currentCard.rank);
+  var rankIndex = null;
+
+  if (currentRankPossition === 12) {
+    rankIndex = 0;
+  }
+  else {
+    rankIndex = currentRankPossition + 1;
+  }
+
+  return {
+    suit: currentCard.suit,
+    rank: ranks[rankIndex]
+  };
+}
 
 app.listen(3000);
